e3fd41d7db963070c55dc0d4e527adc1627ad646
msibia <msibia@outlook.com>
2020-03-25 15:58:27 +0100

    Js_Assignment
    Javascript first task

---
+++ Java_assignment/script.js
@@ -0,0 +1,12 @@
+var name = "Dorothy"
+console.log('My name is ' + name);
+
+var courses = ["Php", "Javascript", "html", "css"]
+console.log("I'm taking the following courses: " + courses);
+
+var evenNumbers = [];
+for(var i = 2; i <=200; i = i + 2) {
+	evenNumbers.push(i);
+}
+
+console.log('Even numbers between 1 and 200 are: ' + evenNumbers);
